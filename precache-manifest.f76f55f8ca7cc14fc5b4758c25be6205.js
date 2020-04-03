604066f986024d021c7ec8702d3569c8349ae02b
justyw0w <justyw0w@mail.ru>
2020-04-03 17:17:39 +0700

    Updates

---
+++ precache-manifest.f76f55f8ca7cc14fc5b4758c25be6205.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4c4637cce44c8967df5af8edce17924",
+    "url": "/rick-morty/index.html"
+  },
+  {
+    "revision": "15dd7a3462b224246de9",
+    "url": "/rick-morty/static/css/main.b7a327f1.chunk.css"
+  },
+  {
+    "revision": "334fc52fd204378e50ab",
+    "url": "/rick-morty/static/js/2.2cff1daa.chunk.js"
+  },
+  {
+    "revision": "15dd7a3462b224246de9",
+    "url": "/rick-morty/static/js/main.8ccd4d96.chunk.js"
+  },
+  {
+    "revision": "a079f2fc39b5e73e24f1",
+    "url": "/rick-morty/static/js/runtime~main.6bffb73a.js"
+  }
+]);
